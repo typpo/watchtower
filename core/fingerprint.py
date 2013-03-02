74ef20eee89dde482d234aaa256920512209b204
Ian <typppo@gmail.com>
2013-03-02 14:37:25 -0800

    get_fingerprints

--- core/fingerprint.py
+++ core/fingerprint.py
@@ -3,11 +3,11 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time
 
-def get_fingerprint(url, selector):
+def get_fingerprints(url, selectors):
   browser = webdriver.Firefox() # Firefox for now
   browser.get(url) # Load page
 
-# TODO selector needs to be escaped
+  # TODO selector needs to be js escaped
 
   # inject jquery
   f = open('jquery.js')
@@ -15,24 +15,25 @@ def get_fingerprint(url, selector):
   f.close()
   browser.execute_script(jquery_js)
 
-  # eval some js
+  # check all selectors
+  ret = [get_fingerprint(browser, sel) for sel in selectors]
+
+  browser.close()
+
+  return ret
+
+def get_fingerprint(browser, selector):
   eval_js = """
   var $ = window.jQuery;
   var $el = $('{{ SELECTOR }}');
   return {
-    width: $el.width(),
-    height: $el.height(),
     offset: $el.offset(),
     innerHTML: $el.html(),
     outerHTML: $el.parent().html(),
     computedStyle: window.getComputedStyle($el.get(0))
   };
   """.replace('{{ SELECTOR }}', selector)
-  ret = browser.execute_script(eval_js)
-
-  browser.close()
-
-  return ret
+  return browser.execute_script(eval_js)
 
 if __name__ == '__main__':
-  print get_fingerprint('http://www.google.com', '#lga')
+  print get_fingerprints('http://www.google.com', ['#lga', '#mngb'])
