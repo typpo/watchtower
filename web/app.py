5d90a57cc26d7aa72110ebe7b0e4f80de650ecdc
Kyle Konrad <kyle.t.konrad@gmail.com>
2013-03-02 19:31:37 -0800

    refactor

--- web/app.py
+++ web/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, request, redirect, session, url_for, render_template, Response, g, flash, Markup, jsonify
 from flask.ext.openid import OpenID
-from flask.ext.sqlalchemy import SQLAlchemy
 import urllib
 import urlparse
 import json
@@ -11,45 +10,21 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from core.models import User
+from core.models import Element, Page, User
+from core.database import db
 
 app = Flask(__name__)
 app.secret_key = 'not a secret key'
+db.init_app(app)
 oid = OpenID(app, 'temp/openid')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/watchtower.db'
-db = SQLAlchemy(app)
-
-class Element(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  selector = db.Column(db.String(255), unique=True)
-  page_id = db.Column(db.Integer, db.ForeignKey('page.id'))
-  page = db.relationship('Page',
-                         backref=db.backref('elements', lazy='dynamic'))
-
-  def __init__(self, selector, page):
-    self.selector = selector
-    self.page = page
-
-  def __repr__(self):
-    return '<Element %r>' % self.selector
-
-class Page(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  url = db.Column(db.String(1024))
-  blob = db.Column(db.Text)
-  
-  def __init__(self, url, blob):
-    self.url = url
-    self.blob = blob
-
-  def __repr__(self):
-    return '<Page %r>' % self.url
 
 
 @app.route("/")
 def index():
   app.logger.debug(g.user)
   pages = Page.query.all()
+  app.logger.debug(pages)
   return render_template('index.html', user=g.user, pages=pages)
 
 @app.route('/watch', methods=['POST'])
@@ -73,6 +48,7 @@ def watch():
   db.session.add(page)
   for element in elements:
     db.session.add(element)
+  db.session.commit()
 
   return jsonify(success='ok')
 
@@ -126,8 +102,6 @@ def test():
   return render_template('test_goog.html',
       random=random.randint(50, 1000),
       randcolor=[random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)])
-
-db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', use_reloader=True)
