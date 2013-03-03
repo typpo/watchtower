89a9f34e66f82ef38cc7b5ce6817ad22418e3894
Ian <typppo@gmail.com>
2013-03-02 22:00:26 -0800

    nothing

--- core/cron.py
+++ core/cron.py
@@ -7,7 +7,10 @@ from datetime import datetime, timedelta
 pages = Page.query.filter(Page.next_check < datetime.now())
 
 print len(pages), 'pages due for checking'
-print pages
 
 for page in pages:
+
+  # TODO check page
+
   page.next_check = datetime.now() + timedelta(minutes=page.frequency)
+  page.save()
