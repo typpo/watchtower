d61d805ad9317f0ec48854819a01b125088cb70b
Kyle Konrad <kyle.t.konrad@gmail.com>
2013-03-03 18:34:30 -0800

    styling :(((

--- core/utils.py
+++ core/utils.py
@@ -15,7 +15,8 @@ def login_required(f):
   @wraps(f)
   def decorated_function(*args, **kwargs):
     if g.user is None:
-      return redirect(url_for('login', next=request.url))
+      flash('You must log in to see this page')
+      return redirect(request.referrer or '/')
     return f(*args, **kwargs)
   return decorated_function
 
@@ -33,11 +34,14 @@ def must_own_page(f):
     page = Page.query.filter_by(id=page_id).first()
     if not page:
       flash('You do not have permission to access this page')
-    if page.user_id is None:
+      print request.referrer
+      return redirect(request.referrer or '/')
+    elif not page.user_id:
       page.user_id = g.user.id
       db.session.add(page)
     elif page.user_id != g.user.id:
       flash('You do not have permission to access this page')
-      return redirect(request.referrer)
+      print request.referrer
+      return redirect(request.referrer or '/')
     return f(page)
   return decorated_function
