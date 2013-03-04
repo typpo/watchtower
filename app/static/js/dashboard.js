89c7f4d9450ee0abf90bcfab2a545235a86a2d0c
Kyle Konrad <kyle.t.konrad@gmail.com>
2013-03-04 13:27:26 -0800

    global to prevent loading feed twice

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,10 +1,16 @@
-$.ajax('/news').done(function(data) {
-  $feed = $('#feed');
-  $.each(data.reddit, function(page, posts) {
-    $.each(posts, function(i, item) {
-      if (i < 5) {
-        $feed.append('<a href="' + item[0]  +'">' + item[1] + '</a><br>');
-      }
+(function() {
+  if (typeof dashboard_loaded == 'undefined') {
+    dashboard_loaded = true;
+
+    $.ajax('/news').done(function(data) {
+      $feed = $('#feed');
+      $.each(data.reddit, function(page, posts) {
+        $.each(posts, function(i, item) {
+          if (i < 5) {
+            $feed.append('<a href="' + item[0]  +'">' + item[1] + '</a><br>');
+          }
+        });
+      });
     });
-  });
-});
+  }
+})();
