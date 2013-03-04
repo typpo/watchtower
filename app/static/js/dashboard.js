c2e41fb50ec4448d7caa51d9f946d91c52c5f622
Andy Kearney <akearney@room77.com>
2013-03-04 13:47:57 -0800

    show/hide news

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,6 +1,7 @@
 (function() {
     $.ajax('/news').done(function(data) {
       var $feed = $('#feed');
+      $feed.append('<p><h5>Social News on Sites You\'re Tracking</h5></p>');
       $.each(data.reddit, function(page, posts) {
         $.each(posts, function(i, item) {
           if (i < 5) {
