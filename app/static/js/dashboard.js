4bba666aae255aa5b7179e3e33718f646fd0cef1
Ian <typppo@gmail.com>
2013-03-04 13:35:50 -0800

    Remove some duplicate scripts

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,14 +1,12 @@
 $.ajax('/news').done(function(data) {
-  $feed = $('#feed');
+  var $feed = $('#feed');
   $.each(data.reddit, function(page, posts) {
-    if (!$('.' + page.name).length) {
-      $feed.append('<span class="' + page.name + '">');
-      $.each(posts, function(i, item) {
-        if (i < 5) {
-          $feed.append('<a href="' + item[0]  +'">' + item[1] + '</a><br>');
-        }
-      });
-      $feed.append('</span>');
-    }
+    $feed.append('<span class="' + page.name + '">');
+    $.each(posts, function(i, item) {
+      if (i < 5) {
+        $feed.append('<a href="' + item[0]  +'">' + item[1] + '</a><br>');
+      }
+    });
+    $feed.append('</span>');
   });
 });
