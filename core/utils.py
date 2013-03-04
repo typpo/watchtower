79273944120a97b0653e6402dff0812458bde640
Ian <typppo@gmail.com>
2013-03-03 17:38:51 -0800

    fix utils refactor

--- core/utils.py
+++ core/utils.py
@@ -2,6 +2,8 @@ import requests
 import socket
 from functools import wraps
 from flask import g, request, redirect, url_for, flash
+from core.models import Element, Version, Page, User
+from core.database import db
 
 def get_blob(url):
   return requests.get(url).text
