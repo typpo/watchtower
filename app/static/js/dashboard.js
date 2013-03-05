143d3c694c6409e1de1ceeffe17e1bc75e370bd0
Kyle Konrad <kyle.t.konrad@gmail.com>
2013-03-05 14:45:07 -0800

    newsfeed formatting

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,38 +1,41 @@
 (function() {
-    $.ajax('/news').done(function(data) {
-      $('#feed_container').prepend('<p><h5>Social News on Sites You\'re Tracking</h5></p>');
-      var $feed = $('#feed');
-      if (data.reddit && data.reddit.length)
-        $feed.append('<h6> Reddit Feed </h6>')
-      $.each(data.reddit, function(page, posts) {
-        $feed.append('<ul><p>' + page + '</p>');
-        $.each(posts, function(i, item) {
-          if (i < 5) {
-            $feed.append('<li><a href="' + item[0]  +'">' + item[1] + '</a></li>');
-          }
-        });
-        $feed.append('</ul>');
-      });
-
-      $twitter = $('#twitter');
-      $twitter.append(
-        '<h6> Twitter Feed </h6>' +
-        '<dl>' +
-          '<form id=twitter_form action="{{ url_for("index") }}" method="post">' +
-          '<input type="text" name=addtweets placeholder="Topic">' +
-          '<input type="submit" class="btn" value="Add Topic">' +
-          '</form>' +
-        '</dl>');
-      console.log(data);
-      $.each(data.feed, function(i) {
-        var interest = data.feed[i];
-        $twitter.append('<ul><p>' + decodeURIComponent(interest.query) + '</p>');
-        $.each(interest.results, function(t) {
-          var tweet = interest.results[t];
-          if (t < 5)
-            $twitter.append('<li>' + tweet.text + '</li>');
-        });
-        $feed.append('</ul>');
-      });
+  $.ajax('/news').done(function(data) {
+    var $feed = $('#news');
+    if (Object.keys(data.reddit).length) {
+      $feed.append('<h3>Reddit</h3>')
+    }
+    $.each(data.reddit, function(page, posts) {
+      $feed.append(tmpl("news_page_tmpl", {
+        page: page,
+        page_id: page.replace(' ', '_'),
+        inner: $.map(posts, function(post) {
+          return tmpl("news_story_tmpl", {
+            link: post[0],
+            headline: post[1]
+          });
+        }).join('')
+      }));
     });
+                         
+    $twitter = $('#twitter');
+    $twitter.append(
+      '<h6> Twitter Feed </h6>' +
+        '<dl>' +
+        '<form id=twitter_form action="{{ url_for("index") }}" method="post">' +
+        '<input type="text" name=addtweets placeholder="Topic">' +
+        '<input type="submit" class="btn" value="Add Topic">' +
+        '</form>' +
+        '</dl>');
+    console.log(data);
+    $.each(data.feed, function(i) {
+      var interest = data.feed[i];
+      $twitter.append('<ul><p>' + decodeURIComponent(interest.query) + '</p>');
+      $.each(interest.results, function(t) {
+        var tweet = interest.results[t];
+        if (t < 5)
+          $twitter.append('<li>' + tweet.text + '</li>');
+      });
+      $feed.append('</ul>');
+    });
+  });
 })();
