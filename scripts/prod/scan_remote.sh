b0b6a7250ccf90ffa0c740ff931ba05238bee408
Ian <typppo@gmail.com>
2013-10-20 23:04:12 +0000

    scan remote working

--- scripts/prod/scan_remote.sh
+++ scripts/prod/scan_remote.sh
@@ -5,8 +5,11 @@ source bin/activate
 
 # Add instance
 cd scripts
-python add_instance.py
+#python add_instance.py
 
 # TODO run on instance
+host=`python get_scan_instance.py`
+
+ssh -i ~/.ssh/watchtower-key.pem $host "./watchtower/scripts/run_cron.sh"
 
 # TODO terminate instance
