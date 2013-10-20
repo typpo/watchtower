6bf6fd0de2011de5937348cc4a11ce7e18734236
Ian <typppo@gmail.com>
2013-10-20 22:59:12 +0000

    get scan instanceK

---
+++ scripts/get_scan_instance.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import boto
+import sys
+
+SCAN_SEC_GROUP = 'watchtower-scan'
+
+conn = boto.connect_ec2()
+
+reservations = conn.get_all_instances()
+instances = [i for r in reservations for i in r.instances]
+
+for instance in instances:
+  for g in instance.groups:
+    if g.name == SCAN_SEC_GROUP:
+      print instance.public_dns_name
+      sys.exit(0)
