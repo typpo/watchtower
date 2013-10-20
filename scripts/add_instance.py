b78f425cec4273155f35b646292dc65bfd84fc74
Kyle Konrad <kyle.t.konrad@gmail.com>
2013-10-20 14:01:12 -0700

    remove some things from add_instance

--- scripts/add_instance.py
+++ scripts/add_instance.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 """
-Copyright 2012, Room 77, Inc.
 @author Kyle Konrad
 
 Class and driver for launching AWS instances
@@ -28,10 +27,8 @@ class Launcher:
 
   def update_cluster_config(self):
     """
-    call the script that updates the cluster config file and local DNS
+    call the script that updates the cluster config file
     """
-    subprocess.call(['%s/prod_config/ec2corp.pl --workdir %s --force' \
-                     % (Launcher.BASE_DIR, Launcher.WORKING_DIR)], shell=True)
     subprocess.check_call('cp %s/config.ip.json %s/prod_config/config.ip.json' \
                           % (Launcher.WORKING_DIR, Launcher.BASE_DIR), shell=True)
 
@@ -40,21 +37,21 @@ class Launcher:
     put hypen seperated list of server_types in order
     does not handle 'staging-' prefix
     """
-    server_types = server_type.split(Launcher.SERVER_TYPE_DELIMITER)
+    server_types = server_type.split(launcher.server_type_delimiter)
     if 'staging' in server_types:
-      raise ValueError('invalid server type: "staging"')
-    return Launcher.SERVER_TYPE_DELIMITER.join(server_types)
+      raise valueerror('invalid server type: "staging"')
+    return launcher.server_type_delimiter.join(server_types)
 
   def get_next_serial_num(self, zone, server_type):
     """
     read server configuration file to figure out next serial number for server
-    @param zone        - AWS zone with optional staging prefix
+    @param zone        - aws zone with optional staging prefix
     @param server_type - server name describing environment and what services will run on it - not including staging prefix
                          e.g., meta or index-book
     """
     # get current config
     self.update_cluster_config()
-    with open('%s/%s' % (Launcher.WORKING_DIR, Launcher.CONFIG_FILE_NAME)) as json_file:
+    with open('%s/%s' % (launcher.working_dir, launcher.config_file_name)) as json_file:
       cluster_config = json.load(json_file)  # {zone => {type => ips}}
 
     # nothing in this zone of this type
@@ -62,21 +59,9 @@ class Launcher:
       return 1
     zone_config = cluster_config[zone]  # {type => ips}
 
-    # filter deprecated (or non-deprecated from the list)
-    for stype in zone_config:
-      ip_list = zone_config[stype]
-      new_ip_list = []
-      for ip in ip_list:
-        if 'deprecated' in zone_config and \
-           (not self._deprecated and ip in zone_config['deprecated']) or \
-           (self._deprecated and not ip in zone_config['deprecated']):
-          continue
-        new_ip_list.append(ip)
-      zone_config[stype] = new_ip_list
-
     # find machines that are in all server types
     ips = set()
-    for s_type in server_type.split(Launcher.SERVER_TYPE_DELIMITER):
+    for s_type in server_type.split(launcher.server_type_delimiter):
       if s_type not in zone_config.keys():
         return 1
       if len(ips) == 0:
@@ -93,14 +78,14 @@ class Launcher:
       return region + 'a'
 
   def get_block_device_map(self):
-    """Returns the block device mapping for the instance being launched.
+    """returns the block device mapping for the instance being launched.
 
-    Returns:
-      boto.ec2.BlockDeviceMapping: The block device mapping.
+    returns:
+      boto.ec2.blockdevicemapping: the block device mapping.
     """
-    dev_sda1 = boto.ec2.blockdevicemapping.EBSBlockDeviceType()
-    dev_sda1.size = self._disk_size  # Size in Gigabytes.
-    bdm = boto.ec2.blockdevicemapping.BlockDeviceMapping()
+    dev_sda1 = boto.ec2.blockdevicemapping.ebsblockdevicetype()
+    dev_sda1.size = self._disk_size  # size in gigabytes.
+    bdm = boto.ec2.blockdevicemapping.blockdevicemapping()
     bdm['/dev/sda1'] = dev_sda1
     return bdm
 
@@ -109,26 +94,27 @@ class Launcher:
     launch a new ec2 instance
     """
     # get args
-    USER = 'walle'
+    user = 'watchtower'
     # connect to ec2
     try:
       ec2_region = [r for r in boto.ec2.regions() if r.name == self._region][0]
-    except IndexError:
+    except indexerror:
       print >> sys.stderr, 'unknown region: %s' % self._region
       exit(2)
+    print boto.config.items('Credentials')
     ec2_connection = ec2_region.connect()
 
     # reserve instance
     if self._test_mode:
       print "test mode: not launching"
-      instance = None
+      instance = none
     else:
       print "launching %s in %s" % (self._server_type, self._zone)
       reservation = ec2_connection.run_instances(self._ami, key_name=self._key_name,
           instance_type=self._instance_type, security_groups=self._security_groups,
           placement=self._zone, block_device_map=self.get_block_device_map())
       instance = reservation.instances[0]
-      while True:  # wait for the instance to come up
+      while true:  # wait for the instance to come up
         time.sleep(5)
         if instance.update() == 'running':
           break
@@ -139,32 +125,21 @@ class Launcher:
     full_zone = '%s%s' % (staging_prefix, self._zone)
     type_serial_num = self.get_next_serial_num(full_zone, self._server_type)
     if (type_serial_num > 99):
-      raise ValueError("serial num %d is too long" % type_serial_num)
+      raise valueerror("serial num %d is too long" % type_serial_num)
     name = '%s%s%02d' % (staging_prefix, self.canonicalize_server_type(self._server_type), type_serial_num)
     print name
     if not self._test_mode:
-      instance.add_tag('Name', name)
-    # update local DNS
+      instance.add_tag('name', name)
+    # update local dns
     self.update_cluster_config()
 
     # with the new ip file push
-    ip = unicodedata.normalize('NFKD', instance.private_ip_address).encode('ascii', 'ignore')
+    ip = unicodedata.normalize('nfkd', instance.private_ip_address).encode('ascii', 'ignore')
 
     # wait until ssh is available
     print "waiting until ssh is avaiable for host"
-    subprocess.check_call(['./scripts/push custom:hn\=%s wait_until_ssh_available' % ip], shell=True)
-
-    # Setup the new machine.
-    subprocess.check_call(\
-        ['%s/scripts/push custom:hn\=%s setup_new_machine:'
-         'target_env=%s,type=%s' % (Launcher.BASE_DIR, ip, self._environment,
-          self._server_type)], shell=True)
-
-    # Everything done.
-    subprocess.check_call(\
-        ['%s/scripts/push custom:hn\=%s mark_machine_successfully_configured' %
-         (Launcher.BASE_DIR, ip)], shell=True)
-
+    #todo(kk)
+    #subprocess.check_call(['./scripts/push custom:hn\=%s wait_until_ssh_available' % ip], shell=true)
 
   def __init__(self, *args, **kwargs):
     """
@@ -173,34 +148,26 @@ class Launcher:
                     optional arguments: key_name, instance_type, ami, security_group, zone
     """
     # get args
-    self._environment = kwargs['environment']
-    self._server_type = kwargs['type']
     self._region = kwargs.get('region', 'us-east-1')
     self._zone = kwargs.get('zone', self.default_zone(self._region))
-    self._key_name = kwargs.get('key_name', 'optrip_aws')
+    self._key_name = kwargs.get('key_name', 'watchtower-key')
     self._instance_type = kwargs.get('instance_type', 'm1.large')
-    self._ami = kwargs.get('ami', 'ami-d52f63bc')
+    self._server_type = kwargs.get('server_type', 'scan')
+    self._ami = kwargs.get('ami', 'tood(kk)')
     self._disk_size = kwargs.get('disk_size', 50)
-    self._security_groups = [kwargs.get('security_group', 'r77_internal')]
-    # add the r77_elb security group if it is apache
-    if self._server_type == 'apache' and not 'r77_elb' in self._security_groups:
-      self._security_groups.append('r77_elb')
+    self._security_groups = [kwargs.get('security_group', 'watchtower_scan')]
     self._test_mode = kwargs.get('test', False)
-    self._deprecated = kwargs.get('deprecated', False)
-    self._deprecated_role = "deprecated" if self._deprecated else ""
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='Add an EC2 instance')
-  parser.add_argument('environment', help='staging or prod')
-  parser.add_argument('type', help='e.g., meta or index-book')
-  # NOTE - dashes in arguments mapped to underscores
-  parser.add_argument('--deprecated', help='push to the deprecated machines', default=False, action='store_true')
+  parser = argparse.ArgumentParser(description='add an ec2 instance')
+  # note - dashes in arguments mapped to underscores
   parser.add_argument('--region', help='e.g., us-east-1')
-  parser.add_argument('--key-name', help='e.g., optrip_aws')
-  parser.add_argument('--instance-type', help='e.g., m1.large', required=True)
+  parser.add_argument('--key-name', help='e.g., watchtower-key')
+  parser.add_argument('--instance-type', help='e.g., m1.large')
+  parser.add_argument('--server-type', help='e.g., scan')
   parser.add_argument('--ami', help='e.g., ami-d52f63bc')
   parser.add_argument('--disk_size', help='size in GB. e.g., 50')
-  parser.add_argument('--security-group', help='e.g., r77_internal')
+  parser.add_argument('--security-group', help='e.g., watchtower_scan')
   parser.add_argument('--zone', help='e.g., us-east-1b')
   parser.add_argument('--test', help="testing mode - don't launch instance", default=False, action='store_true')
 
