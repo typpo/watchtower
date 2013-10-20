39b991608e64f9672178d7ff6b4b1241cb668709
Ian <typppo@gmail.com>
2013-10-20 22:45:11 +0000

    a way for scan server to talk to main server

--- core/database.py
+++ core/database.py
@@ -3,20 +3,24 @@ The database
 """
 import sys
 import os
+  import socket
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-#from core.utils import is_production
+
 def is_production():
-  # I'm so bad why doesn't this work??
-  import socket
   return socket.gethostname().endswith('gowatchtower.com')
+
+def is_scan():
+  return socket.gethostname().endswith('watchtower-scan')
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.secret_key = 'not a secret key'
 if is_production():
   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:jackathon@localhost/watchtower'
+elif is_scan():
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:jackathon@75.101.157.206/watchtower'
 else:
   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/watchtower/watchtower.db'
 db.init_app(app)
