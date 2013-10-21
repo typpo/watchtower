5d248ce597d4636cc9a439a6d0a3fd4a838680ab
Ian <typppo@gmail.com>
2013-10-21 17:17:45 +0000

    get_scan_instance waits for available

--- scripts/get_scan_instance.py
+++ scripts/get_scan_instance.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
 import boto
+import time
 import sys
 
 SCAN_SEC_GROUP = 'watchtower-scan'
 
 conn = boto.connect_ec2()
 
-reservations = conn.get_all_instances()
-instances = [i for r in reservations for i in r.instances]
+while True:
+  print 'Waiting for scan instance...'
+  reservations = conn.get_all_instances()
+  instances = [i for r in reservations for i in r.instances]
 
-for instance in instances:
-  for g in instance.groups:
-    if g.name == SCAN_SEC_GROUP:
-      if instance.public_dns_name != '':
+  for instance in instances:
+    for g in instance.groups:
+      if g.name == SCAN_SEC_GROUP:
         # fml
-        print instance.public_dns_name
+        if instance.public_dns_name != '':
+          print instance.public_dns_name
+          sys.exit(0)
+  time.sleep(5)
