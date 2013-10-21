a597e1ce8665502d01021bb3ac6ea7f6f2734433
Ian <typppo@gmail.com>
2013-10-21 02:00:38 +0000

    activate venv

--- scripts/prod/remote_scan.sh
+++ scripts/prod/remote_scan.sh
@@ -7,11 +7,12 @@ source bin/activate
 cd scripts
 python add_instance.py --instance-type 'm1.large'
 
-sleep 10
+sleep 40
 
 # run on instance
 host=`python get_scan_instance.py`
-ssh -i ~/.ssh/watchtower-key.pem $host "cd watchtower && git pull && ./scripts/run_cron.sh"
+echo "Connecting to host $host..."
+ssh -i ~/.ssh/watchtower-key.pem $host "cd ~/watchtower && git pull && source bin/activate && ./scripts/run_cron.sh"
 
 # terminate instance
 python terminate_instance.py  --instance_name scan00
