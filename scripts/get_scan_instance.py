56f8b411cb26da955abac605241380e0c8882bc8
Ian <typppo@gmail.com>
2013-10-21 17:39:19 +0000

    get_scan_instance tells if host is accepting connections

--- scripts/get_scan_instance.py
+++ scripts/get_scan_instance.py
@@ -6,6 +6,33 @@ import sys
 
 SCAN_SEC_GROUP = 'watchtower-scan'
 
+import socket
+
+def DoesServiceExist(host, port):
+  # stackoverflow
+  captive_dns_addr = ""
+  host_addr = ""
+
+  try:
+    captive_dns_addr = socket.gethostbyname("BlahThisDomaynDontExist22.com")
+  except:
+    pass
+
+  try:
+    host_addr = socket.gethostbyname(host)
+
+    if (captive_dns_addr == host_addr):
+        return False
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+    s.connect((host, port))
+    s.close()
+  except:
+    return False
+
+  return True
+
 conn = boto.connect_ec2()
 
 while True:
@@ -16,8 +43,12 @@ while True:
     for g in instance.groups:
       if g.name == SCAN_SEC_GROUP:
         # fml
-        instance.update()
-        if instance.public_dns_name != '' and instance.state == 'running':
-          print instance.public_dns_name
-          sys.exit(0)
+        status = instance.update()
+        if instance.public_dns_name != '' and status == 'running':
+          host = instance.public_dns_name
+          if DoesServiceExist(host, 22):
+            print host
+            sys.exit(0)
   time.sleep(5)
+
+
