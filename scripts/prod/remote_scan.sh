2ca1e6d985f4dc3403b93290c01af713f45da2ff
Ian <typppo@gmail.com>
2013-10-21 01:50:22 +0000

    fixed a bug

--- scripts/prod/remote_scan.sh
+++ scripts/prod/remote_scan.sh
@@ -5,11 +5,13 @@ source bin/activate
 
 # Add instance
 cd scripts
-python add_instance.py
+python add_instance.py --instance_type m1.large
+
+sleep 10
 
 # run on instance
 host=`python get_scan_instance.py`
-ssh -i ~/.ssh/watchtower-key.pem $host "./watchtower/scripts/run_cron.sh"
+ssh -i ~/.ssh/watchtower-key.pem $host "cd watchtower && git pull && ./scripts/run_cron.sh"
 
 # terminate instance
 python terminate_instance.py  --instance_name scan00
